87c16ba1d19571d9692fa90757d978170ad6ab0b
Sean Soper <sean.soper@gmail.com>
2011-12-27 10:57:19 -0500

    init

---
+++ BNB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BNB
+//
+//  Created by Sean Soper on 12/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
